7b4f387dc638075e17b0aa2ac9b7aae75d807aa0
stephen22otieno <stephenombiro22@gmail.com>
2018-10-30 18:15:42 +0300

    add change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Http, Response} from '@angular/http';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Github';
+
+  constructor(private http:Http){}
+  userName="";
+  githubData:any="";
+
+
+MoreInfo(){
+this.http.get("https://api.github.com/users/"+this.userName+"?access_token="+environment.apiUrl)
+.subscribe(
+  (response:Response)=>{
+    const userData=response.json();
+    this.githubData=userData;
+
+    console.log(userData);
+  }
+)
+
+
+
+
+}
 }
